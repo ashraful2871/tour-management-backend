c3d1e7f41ee2a4a9127638f2fa0e95529a7c6ce2
ashraful2871 <ashrafulislam2758@gmail.com>
2025-07-09 15:05:53 +0600

    setup eslint and env

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,4 @@
-import { Server } from "http";
 import express, { Request, Response } from "express";
-import mongoose from "mongoose";
 const app = express();
 app.get("/", async (req: Request, res: Response) => {
   res.status(200).json({
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
+/* eslint-disable no-console */
 import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
+import { envVars } from "./app/config/env";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 let server: Server;
-
 const startServer = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://book:book@cluster0.jq7qb.mongodb.net/tour-DB?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    await mongoose.connect(envVars.DB_URL);
     console.log("connected to db");
 
-    server = app.listen(5000, () => {
-      console.log("server is listening on port 5000");
+    server = app.listen(envVars.PORT, () => {
+      console.log(`server is listening on port ${envVars.PORT}`);
     });
   } catch (error) {
     console.log(error);
