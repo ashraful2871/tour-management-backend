0d2b0ccdc7efbc3be24e9c338193199a21e0fa0b
ashraful2871 <ashrafulislam2758@gmail.com>
2025-07-10 19:02:01 +0600

    Implement validateRequest Middleware

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import { userController } from "./user.controller";
+import { createUserZodSchema } from "./user.validation";
+import { validatedRequest } from "../../middlewares/validateRequest";
 
 const router = Router();
-router.post("/register", userController.createUser);
+router.post(
+  "/register",
+  validatedRequest(createUserZodSchema),
+  userController.createUser
+);
 router.get("/all-users", userController.getAllUSers);
 
 export const userRoutes = router;
