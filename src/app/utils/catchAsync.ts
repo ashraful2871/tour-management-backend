754ee6a4df92103e76c9cb6e5ff8ec3ee39cae61
ashraful2871 <ashrafulislam2758@gmail.com>
2025-07-15 10:45:28 +0600

    Create model for Tour and Tour Types

--- src/app/utils/catchAsync.ts
+++ src/app/utils/catchAsync.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from "express";
+import { envVars } from "../config/env";
 
 type asyncHandler = (
   req: Request,
@@ -12,7 +13,9 @@ export const catchAsync =
   (fn: asyncHandler) =>
   async (req: Request, res: Response, next: NextFunction) => {
     Promise.resolve(fn(req, res, next)).catch((err: any) => {
-      console.log(err);
+      if (envVars.NODE_ENV === "development") {
+        console.log(err);
+      }
       next(err);
     });
   };
