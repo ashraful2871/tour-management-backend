586a5b94cfb5997b9b5fa143f012560fa4068e1b
ashraful2871 <ashrafulislam2758@gmail.com>
2025-07-17 18:42:38 +0600

    add env exam pple file data

---
+++ index.ts
@@ -0,0 +1,31 @@
+// PORT=5000
+// DB_URL=mongodb+srv://book:book@cluster0.jq7qb.mongodb.net/tour-DB?retryWrites=true&w=majority&appName=Cluster0
+// NODE_ENV=development
+// # NODE_ENV=production
+
+// # JWT
+// JWT_ACCESS_SECRET=access_secret
+// JWT_ACCESS_EXPIRES=1d
+
+// # JWT REFRESH
+// JWT_REFRESH_SECRET=refres_secret
+// JWT_REFRESH_EXPIRES=30d
+
+// # BCRYPT
+// BCRYPT_SALT_ROUND=10
+
+// # SUPER ADMIN
+// SUPER_ADMIN_EMAIL=super@gmail.com
+// SUPER_ADMIN_PASSWORD=12345678
+
+// # google
+
+// GOOGLE_CLIENT_SECRET=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
+// GOOGLE_CLIENT_ID=798637532562-c3r1l5grvghgevr550rqgkvjcdo1n2at.apps.googleusercontent.com
+// GOOGLE_CALLBACK_URL=/api/v1/auth/google/callback
+
+// #  express session
+// EXPRESS_SESSION_SECRET=express_session
+
+// # frontend url
+// FRONTEND_URL=http://localhost:5173
