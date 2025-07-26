6ad8c6e0a356eae2d4882b4a7df3241504c01dd8
ashraful2871 <ashrafulislam2758@gmail.com>
2025-07-26 16:25:19 +0600

    Create a function to generate and send OTP by Email

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -42,6 +42,10 @@ interface EnvConfig {
     SMTP_PASS: string;
     SMTP_FROM: string;
   };
+  REDIS_HOST: string;
+  REDIS_PORT: string;
+  REDIS_USERNAME: string;
+  REDIS_PASSWORD: string;
 }
 const loadEnvVariable = (): EnvConfig => {
   const requiredEnvVariables: string[] = [
@@ -78,6 +82,10 @@ const loadEnvVariable = (): EnvConfig => {
     "SMTP_USER",
     "SMTP_PASS",
     "SMTP_FROM",
+    "REDIS_HOST",
+    "REDIS_PORT",
+    "REDIS_USERNAME",
+    "REDIS_PASSWORD",
   ];
   requiredEnvVariables.forEach((key) => {
     {
@@ -128,6 +136,10 @@ const loadEnvVariable = (): EnvConfig => {
       SMTP_PASS: process.env.SMTP_PASS as string,
       SMTP_FROM: process.env.SMTP_FROM as string,
     },
+    REDIS_HOST: process.env.REDIS_HOST as string,
+    REDIS_PORT: process.env.REDIS_PORT as string,
+    REDIS_USERNAME: process.env.REDIS_USERNAME as string,
+    REDIS_PASSWORD: process.env.REDIS_PASSWORD as string,
   };
 };
 export const envVars = loadEnvVariable();
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -6,6 +6,7 @@ import { tourType } from "../modules/tour-type/tourType.route";
 import { TourRoutes } from "../modules/tour/tour.route";
 import { BookingRoutes } from "../modules/booking/booking.route";
 import { PaymentRoutes } from "../modules/payment/payment.route";
+import { OtpRoutes } from "../modules/otp/otp.route";
 export const router = Router();
 
 const moduleRoutes = [
@@ -36,6 +37,10 @@ const moduleRoutes = [
   {
     path: "/payment",
     route: PaymentRoutes,
+  },
+  {
+    path: "/otp",
+    route: OtpRoutes,
   },
 ];
 
