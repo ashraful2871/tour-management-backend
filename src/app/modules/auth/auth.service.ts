c987ae9dfd3393469780d841dfa2eacf473c24c0
ashraful2871 <ashrafulislam2758@gmail.com>
2025-08-08 15:45:26 +0600

    configure cors origin

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -5,54 +5,56 @@ import AppError from "../../../erroralpers/appError";
 import httpStatus from "http-status-codes";
 import { User } from "../user/user.model";
 import bcryptjs from "bcryptjs";
-import { createNewAccessTokenWithNewToken } from "../../utils/userTokens";
+import {
+  createNewAccessTokenWithNewToken,
+  createUserTokens,
+} from "../../utils/userTokens";
 import { JwtPayload } from "jsonwebtoken";
 import { envVars } from "../../config/env";
-import { IAuthProvider, IsActive } from "../user/user.interface";
+import { IAuthProvider, IsActive, IUser } from "../user/user.interface";
 import jwt from "jsonwebtoken";
 import { sendEmail } from "../../utils/sendEmail";
 
-// const credentialsLogin = async (payload: Partial<IUser>) => {
-//   const { email, password } = payload;
-//   const isUserExist = await User.findOne({ email });
+const credentialsLogin = async (payload: Partial<IUser>) => {
+  const { email, password } = payload;
+  const isUserExist = await User.findOne({ email });
 
-//   if (!isUserExist) {
-//     throw new AppError(httpStatus.BAD_REQUEST, "user Dose not Exist");
-//   }
+  if (!isUserExist) {
+    throw new AppError(httpStatus.BAD_REQUEST, "user Dose not Exist");
+  }
 
-//   const isPasswordMatch = await bcryptjs.compare(
-//     password as string,
-//     isUserExist.password as string
-//   );
-//   if (!isPasswordMatch) {
-//     throw new AppError(httpStatus.BAD_REQUEST, "Incorrect  Password");
-//   }
-//   // const jwtPayload = {
-//   //   userID: isUserExist._id,
-//   //   email: isUserExist.email,
-//   //   role: isUserExist.role,
-//   // };
+  const isPasswordMatch = await bcryptjs.compare(
+    password as string,
+    isUserExist.password as string
+  );
+  if (!isPasswordMatch) {
+    throw new AppError(httpStatus.BAD_REQUEST, "Incorrect  Password");
+  }
+  // const jwtPayload = {
+  //   userID: isUserExist._id,
+  //   email: isUserExist.email,
+  //   role: isUserExist.role,
+  // };
 
-//   // const accessToken = generaToken(
-//   //   jwtPayload,
-//   //   envVars.JWT_ACCESS_SECRET,
-//   //   envVars.JWT_ACCESS_EXPIRES
-//   // );
-//   // const refreshToken = generaToken(
-//   //   jwtPayload,
-//   //   envVars.JWT_ACCESS_SECRET,
-//   //   envVars.JWT_ACCESS_EXPIRES
-//   // );
-//   const userTokens = createUserTokens(isUserExist);
+  // const accessToken = generaToken(
+  //   jwtPayload,
+  //   envVars.JWT_ACCESS_SECRET,
+  //   envVars.JWT_ACCESS_EXPIRES
+  // );
+  // const refreshToken = generaToken(
+  //   jwtPayload,
+  //   envVars.JWT_ACCESS_SECRET,
+  //   envVars.JWT_ACCESS_EXPIRES
+  // );
+  const userTokens = createUserTokens(isUserExist);
 
-//   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-//   const { password: pass, ...rest } = isUserExist.toObject();
-//   return {
-//     accessToken: userTokens.accessToken,
-//     refreshToken: userTokens.refreshToken,
-//     user: rest,
-//   };
-// };
+  const { password: pass, ...rest } = isUserExist.toObject();
+  return {
+    accessToken: userTokens.accessToken,
+    refreshToken: userTokens.refreshToken,
+    user: rest,
+  };
+};
 
 const getNewAccessToken = async (refreshToken: string) => {
   const newAccessToken = await createNewAccessTokenWithNewToken(refreshToken);
